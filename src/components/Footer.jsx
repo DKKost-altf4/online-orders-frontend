7146aa72003bef4df9ab67d7c5921433118db09f
DKKost-altf4 <danil.kostygov@mail.ru>
2025-10-16 01:30:14 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white border-t mt-12">
       <div className="container mx-auto px-4 py-6 text-sm text-gray-500">
-        © {new Date().getFullYear()} OnlineShop — демонстрационный фронтенд. Сделано для интеграции с вашим бэкендом.
+        © {new Date().getFullYear()} 
       </div>
     </footer>
   );
